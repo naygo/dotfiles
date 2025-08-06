2955832cdb7bc5322d602c56ec149100dc05fe5e
Nayla Gomes <nayla.cgs@gmail.com>
2025-08-06 01:10:24 -0300

    deleted unused files; update i3, kitty, picom and add gtk

--- .config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Terminate already running bar instances
-killall -q polybar
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-# Launch Polybar
-polybar mainbar 2>&1 | tee -a /tmp/polybar.log & disown
-echo "Polybar launched..."
